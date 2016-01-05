13677d34343f1a99f6fe7c641ac22f141d76342e
tracyhebrew <tracy@fiftyandfifty.org>
2016-01-05 12:50:59 -0800

    header, footer files

---
+++ wp-content/themes/starter-theme/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+	
+</footer>
+
+	</div><!-- #app -->
+  <?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/starter-theme/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+	<header id="header" class="header">
+		<div class="container">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="header__logo"><?php blog_info('name'); ?></a>
+			
+			<?php 
+			wp_nav_menu( array(
+				'container'      => '',
+				'menu_class'     => 'header__menu menu',
+				'theme_location' => 'main_menu',
+				'link_before'    => '',
+				'link_after'     => '',
+			) ); ?>
+
+			<button type="button" role="button" aria-label="Toggle Navigation" id="header__toggle" class="header__toggle">
+	      <span class="lines"></span>
+	    </button>
+		</div>
+
+	</header>
+
+
+	<div id="app">
